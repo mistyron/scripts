58cd6684f2ba10dc2ab83adcb5248f21a1b1329c
Ron Eggler <ron.eggler@ecoation.com>
2020-07-24 15:22:58 -0700

    couple updates

--- git_describe.sh
+++ git_describe.sh
@@ -1,1 +1,4 @@
-test
+OUTPUT=$(git describe --exact-match --tags $(git log -n1 --pretty='%h')|grep "fatal:")
+if [[ $OUTPUT != "0" ]]; then
+    echo "no tag present"
+fi
--- rosparam_set.sh
+++ rosparam_set.sh
@@ -4,5 +4,6 @@ rosparam set /SYS_PATH/config_path /home/eis/DATA/
 rosparam set /SYS_PATH/site_info_path /home/eis/DATA/site_info
 rosparam set /SYS_PATH/site_tags_path /home/eis/DATA/site_tags
 rosparam set /SYS_PATH/site_alerts_path /home/eis/DATA/site_alerts
+rosparam set /SYS_PATH/site_data_entry_path /home/eis/DATA/site_data_entry
 rosparam set /system_access true
 rosparam set /machine_pincode 0
