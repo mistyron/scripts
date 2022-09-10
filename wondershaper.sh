1bfd9f61c22d9c50611b259194ff7123616a6676
stdcerr <stdcerr@yandex.com>
2022-09-10 12:19:50 -0700

    Added wondershaper script

---
+++ wondershaper.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+IF="enp5s0"
+
+if [ -z $1 ] 
+then
+    echo "clear bandwidth limit"
+    sudo wondershaper -a ${IF} -c;
+else
+    echo "set limit to ${1}"
+    sudo wondershaper -a ${IF} -c;sudo wondershaper -a ${IF} -u ${1} -d ${1}
+fi
