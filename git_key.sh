d9a24cd13ae3090dc40822aa0692dc9f134d06d4
stdcerr <stdcerr@yandex.com>
2022-09-14 07:28:07 -0700

    Fixed up script to accept arguments

--- git_key.sh
+++ git_key.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-GIT_SSH_COMMAND='ssh -i /home/ron/.ssh/id_rsa_stdcerr' git pull
+Help()
+{
+   # Display Help
+   echo "Script will execute git operations using a specifid key"
+   echo
+   echo "Usage: git_key.sh key operaton"
+   echo
+}
+
+if [ "$#" -ne 2 ]; then
+	Help
+	exit
+else
+	GIT_SSH_COMMAND="ssh -i ${1}" git ${2}
+fi
