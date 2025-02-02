eccfb59f9e9e27c340c513ed732adab7a6344f76
MistyRon <ron.eggler@mistywest.com>
2025-02-01 18:33:44 -0800

    added cache options to rclone mounts

--- gdrive_mount.sh
+++ gdrive_mount.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
-rclone mount gdrive: ~/GDrive --daemon
-rclone mount MWgdrive: ~/MWGDrive --daemon
-rclone mount GooglePhotos: ~/GooglePhotos --daemon
+OPTIONS="--daemon \
+--cache-dir /tmp/rclone-cache \
+--vfs-cache-mode full \
+--vfs-cache-max-size 1G \
+--vfs-cache-max-age 24h \
+--vfs-read-chunk-size 32M"
+rclone mount gdrive: ~/GDrive ${OPTIONS}
+rclone mount MWgdrive: ~/MWGDrive ${OPTIONS}
+#rclone mount GooglePhotos: ~/GooglePhotos ${OPTIONS}
